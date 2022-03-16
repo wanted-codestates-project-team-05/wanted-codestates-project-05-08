1b08456190f173dd22b4e9e614bc502e26180790
곽윤구 <dbsrndi123@naver.com>
2022-03-16 16:14:59 +0900

    feat: list.js infinite scroll

--- src/pages/List.js
+++ src/pages/List.js
@@ -6,24 +6,67 @@ const List = () => {
   const loadRef = useRef(null);
   const [page, setPage] = useState(1);
   const [data, setData] = useState([]);
-
-  const url = `/pubdata/pubMapForest.do?numOfRows=10&pageNo=${page}`;
+  const [isLoading, setIsLoading] = useState(false);
+  const [url, setUrl] = useState(`/pubdata/pubMapForest.do?numOfRows=10&pageNo=${page}`);
 
   const getItem = useCallback(async () => {
+    setIsLoading(true);
     const res = await axios.get(url);
-    console.log(res.data);
+    const info = JSON.parse(res.data);
+    if (info.response.length) {
+      setData((prev) => [...prev, ...info.response]);
+    } else {
+      loadRef.current.style.display = 'none';
+    }
+    setIsLoading(false);
   }, [url]);
+
+  const onIntersect = useCallback(
+    (entry, observer) => {
+      if (entry[0].isIntersecting && !isLoading) {
+        observer.unobserve(entry[0].target);
+        setPage((prev) => prev + 1);
+      }
+    },
+    [isLoading]
+  );
 
   useEffect(() => {
     getItem();
   }, [getItem]);
 
+  useEffect(() => {
+    setUrl(`/pubdata/pubMapForest.do?numOfRows=10&pageNo=${page}`);
+  }, [page]);
+
+  useEffect(() => {
+    let observer;
+    if (loadRef.current) {
+      observer = new IntersectionObserver(onIntersect, {
+        threshold: 0.4,
+      });
+      observer.observe(loadRef.current);
+    }
+    return () => observer && observer.disconnect();
+  }, [onIntersect]);
+
   return (
     <div>
+      <header></header>
       <ul>
-        <li></li>
+        {data?.map((item, index) => {
+          return (
+            <li key={index}>
+              <div>
+                <p>{item.fcNm}</p>
+                <p>{item.fcAddr}</p>
+                <p>{item.ref1}</p>
+              </div>
+            </li>
+          );
+        })}
       </ul>
-      <div ref={loadRef}></div>
+      <div ref={loadRef}>{isLoading && 'loading'}</div>
     </div>
   );
 };
