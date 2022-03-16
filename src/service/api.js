8310ccbed200ade0d0bc59b96c43d2e955d4717c
SeongHyun <70502670+Lee-Seonghyun316@users.noreply.github.com>
2022-03-17 01:23:54 +0900

    [Client] / #19 #20 #21 / edit: Network Request Management (#22)
    * Feat: CORS 해결, api 요청 함수 생성
    
    * Fix: 사용 api 변경
    
    * Feat: 데이터 불러온 후 결과값 처리 방법 2가지 추가(return 값 이용, hooks 이용 상태 변경)
    
    * Feat: 성능 개선 위해 useCallback 사용, errorHandling
    
    * Feat: 에러 및 로딩 상태 핸들링
    
    * Rename: 코드 병합 충돌 감소 위해 Sample -> NetworkSample 로 이름변경
    
    * Fix: proxy 설정

---
+++ src/service/api.js
@@ -0,0 +1,23 @@
+export const getRecreationForestData = async (page, perPage) => {
+  const axios = require('axios');
+  const config = {
+    method: 'get',
+    url: `/api/15099285/v1/uddi:57e7fc08-b32c-482d-8dc7-ab02864a70b7?page=${page}&perPage=${perPage}&serviceKey=${process.env.REACT_APP_API_KEY}`,
+    //인증 정보 오류 text url
+    // url: `https://api.odcloud.kr/api/15099285/v1/uddi:57e7fc08-b32c-482d-8dc7-ab02864a70b7?page=${page}&perPage=${perPage}&serviceKey=`,
+  };
+  const response = await axios(config);
+  return response.data;
+};
+
+export const checkErrorStatus = (error) => {
+  const errorString = JSON.stringify(error);
+  const errorObject = JSON.parse(errorString);
+  let errorText = '';
+  if (errorObject.status === 401) {
+    errorText = '인증 정보 오류 :(';
+  } else if (errorObject.status === 500) {
+    errorText = '서버 문제 발생 :(';
+  }
+  return errorText;
+};
