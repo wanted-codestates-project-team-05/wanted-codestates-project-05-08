a6b62aafd2d8f49be90194c37f23242aac9c8e7b
Lee-Seonghyun316 <mn031621019@gmail.com>
2022-03-17 00:44:24 +0900

    Feat: 에러 및 로딩 상태 핸들링

--- src/service/api.js
+++ src/service/api.js
@@ -3,19 +3,21 @@ export const getRecreationForestData = async (page, perPage) => {
   const config = {
     method: 'get',
     url: `https://api.odcloud.kr/api/15099285/v1/uddi:57e7fc08-b32c-482d-8dc7-ab02864a70b7?page=${page}&perPage=${perPage}&serviceKey=${process.env.REACT_APP_API_KEY}`,
+    //인증 정보 오류 text url
     // url: `https://api.odcloud.kr/api/15099285/v1/uddi:57e7fc08-b32c-482d-8dc7-ab02864a70b7?page=${page}&perPage=${perPage}&serviceKey=`,
   };
   const response = await axios(config);
-
   return response.data;
 };
 
 export const checkErrorStatus = (error) => {
   const errorString = JSON.stringify(error);
   const errorObject = JSON.parse(errorString);
+  let errorText = '';
   if (errorObject.status === 401) {
-    console.log('인증 정보가 정확 하지 않음');
+    errorText = '인증 정보 오류 :(';
   } else if (errorObject.status === 500) {
-    console.log('API 서버에 문제가 발생하였음');
+    errorText = '서버 문제 발생 :(';
   }
+  return errorText;
 };
