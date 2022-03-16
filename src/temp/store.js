376cd202c0fbfe9a34dc50f55a5805a734acc84f
kyu <2066apple@naver.com>
2022-03-16 20:46:14 +0900

    Delete: 머지로 인한 파일 삭제

--- src/temp/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import toastListReducer from './toastListSlice';
-
-export const store = configureStore({
-  reducer: {
-		toastList: toastListReducer,
-	},
-})
