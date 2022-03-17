8c571f4a09c1f8cd1c32bef70083eb4392218a2b
곽윤구 <dbsrndi123@naver.com>
2022-03-17 09:36:25 +0900

    feat: use api function

--- src/pages/List.js
+++ src/pages/List.js
@@ -4,29 +4,33 @@ import styled from 'styled-components';
 import ReactLoading from 'react-loading';
 import Modal from '../components/Modal';
 import { ToastList } from '../components/ToastList';
+import { useNavigate } from 'react-router';
+import { getRecreationForestData, checkErrorStatus } from '../service/api';
 
 const List = () => {
+  const navigate = useNavigate();
   const loadRef = useRef(null);
   const [page, setPage] = useState(1);
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [isModal, setIsModal] = useState(false);
   const [singleData, setSingleData] = useState();
-  const [url, setUrl] = useState(
-    `/api/15099285/v1/uddi:57e7fc08-b32c-482d-8dc7-ab02864a70b7?page=${page}&perPage=10&serviceKey=${process.env.REACT_APP_API_KEY}`
-  );
 
   const getItem = useCallback(async () => {
     setIsLoading(true);
-    const res = await axios.get(url);
-    const info = res.data.data;
-    if (info.length) {
-      setData((prev) => [...prev, ...info]);
-    } else {
-      loadRef.current.style.display = 'none';
+    try {
+      const res = await getRecreationForestData(page, 10);
+      const info = res.data;
+      if (info.length) {
+        setData((prev) => [...prev, ...info]);
+      } else {
+        loadRef.current.style.display = 'none';
+      }
+    } catch (err) {
+      checkErrorStatus(err);
     }
     setIsLoading(false);
-  }, [url]);
+  }, [page]);
 
   const onIntersect = useCallback(
     (entry, observer) => {
@@ -41,12 +45,6 @@ const List = () => {
   useEffect(() => {
     getItem();
   }, [getItem]);
-
-  useEffect(() => {
-    setUrl(
-      `/api/15099285/v1/uddi:57e7fc08-b32c-482d-8dc7-ab02864a70b7?page=${page}&perPage=10&serviceKey=${process.env.REACT_APP_API_KEY}`
-    );
-  }, [page]);
 
   useEffect(() => {
     let observer;
@@ -70,7 +68,7 @@ const List = () => {
       <ToastList />
       {isModal && <Modal openModal={setIsModal} data={singleData} />}
       <header className="head">
-        <button>
+        <button onClick={() => navigate(-1)}>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
             <path d="M0 256c0 141.4 114.6 256 256 256s256-114.6 256-256c0-141.4-114.6-256-256-256S0 114.6 0 256zM246.1 129.2C252.1 131.7 256 137.5 256 144v64h96c17.67 0 32 14.33 32 32v32c0 17.67-14.33 32-32 32h-96v64c0 6.469-3.891 12.31-9.875 14.78c-5.984 2.484-12.86 1.109-17.44-3.469l-112-112c-6.248-6.248-6.248-16.38 0-22.62l112-112C233.3 128.1 240.1 126.7 246.1 129.2z" />
           </svg>
